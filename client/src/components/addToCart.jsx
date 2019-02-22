52fcd435c8f61db4d950b7fe206f2d735d24c4d4
G <>
2019-02-22 11:08:22 -0500

    server test

--- client/src/components/addToCart.jsx
+++ client/src/components/addToCart.jsx
@@ -41,7 +41,7 @@ class AddToCart extends React.Component {
 
   getData() {
     var x = window.location.href.split('/')[3]
-    axios.get(`http://18.218.218.14/api/items/${x}`)
+    axios.get(`http://ec2-18-218-218-14.us-east-2.compute.amazonaws.com/api/items/${x}`)
     .then((data) => {
       //console.log(data);
       this.setState({
--- proxy/proxy.js
+++ proxy/proxy.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
 })
 
 app.get('/:id', (req, res) => {
-  axios.get(`http://18.218.218.14/api/items/${req.params.id}`)
+  axios.get(`http://ec2-18-218-218-14.us-east-2.compute.amazonaws.com/api/items/${req.params.id}`)
   .then((data) => {
     res.send(data);
   })
