c57f08db0eb508fbdc2b39cd45171e1ade411de2
G <>
2019-02-23 11:41:34 -0500

    wepack config for optimization

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,17 @@ const config = {
       '.js',
       '.jsx'
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin()//Merge chunks 
+  ]
 }
 
 module.exports = config;
